4dd1dc2a8f13f25a40324ff2113643118a587109
Mohamed Hamida <mhamida292@gmail.com>
2022-05-01 11:05:20 -0700

    Renaming of labs to be more consistent

--- Lab 11/LAB 11 SRC CODE/Lab11.X/build/default/production/ST7735_TFT.p1.d
+++
@@ -1,4 +0,0 @@
- build/default/production/ST7735_TFT.d  \
- build/default/production/ST7735_TFT.p1:  \
- ST7735_TFT.c  \
-ST7735.h 
--- Lab 11/LAB 11 SRC CODE/Lab11.X/build/default/production/lab11.p1.d
+++
@@ -1,7 +0,0 @@
- build/default/production/Lab11.d  \
- build/default/production/Lab11.p1:  \
- Lab11.c  \
-ST7735.h  \
-I2C_Support.h  \
-I2C_Soft.h  \
-Interrupt.h 
